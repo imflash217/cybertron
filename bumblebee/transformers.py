25292c6fae7233d5c244e1f2df95fa762db2e51d
imflash217 <12234745+imflash217@users.noreply.github.com>
2021-10-31 17:21:43 -0400

    helper functions

--- bumblebee/transformers.py
+++ bumblebee/transformers.py
@@ -5,8 +5,8 @@ PyTorch Implementation of Transformers architecture
 
 from inspect import isfunction
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-
 
 ## constants
 DEFAULT_DIM_HEAD = 64
@@ -16,7 +16,7 @@ LAYER_INTERMEDIATES = namedtuple("INTERMEDIATES", ["hiddens", "attn_intermediate
 ## helpers => functions
 
 
-def exists(value):
+def exists(value) -> bool:
     """
     Checks if the value exists or not
     """
@@ -32,7 +32,7 @@ def default(value, default):
     return default() if isfunction(default) else default
 
 
-def cast_tuple(value, depth):
+def cast_tuple(value, depth) -> tuple:
     """
     Casts a value into tuple into the required depth
     """
@@ -79,3 +79,28 @@ class not_equals:
 
 ## helpers => initialization
 
+
+def init_zero_(layer: nn.layer):
+    """inplace intializes the weights and biases of the layer to zero"""
+    nn.init.constant_(layer.weight, 0.0)
+    if exists(layer.bias):
+        nn.init.constant_(layer.bias, 0.0)
+
+
+## helpers => keyword arguments
+
+
+def pick_and_pop(keys, d: dict) -> dict:
+    """
+    pick all (key, val) pair from dictionary 'd' for all keys in 'keys'
+    Then, return a new dict with those picked (key, val) pair
+    """
+    values = list(map(lambda key: d.pop(key), keys))
+    return dict(zip(keys, values))
+
+
+def string_begins_with(prefix: str, string: str) -> bool:
+    """
+    Checks whether the "string" begins with the "prefix"
+    """
+    return string.beginswith(prefix)
