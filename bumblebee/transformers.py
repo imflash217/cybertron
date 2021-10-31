4b4bc914dc3f853e5a3351e3fbc092ee696b3f8b
imflash217 <12234745+imflash217@users.noreply.github.com>
2021-10-31 16:51:28 -0400

    helper classes

--- bumblebee/transformers.py
+++ bumblebee/transformers.py
@@ -13,7 +13,7 @@ DEFAULT_DIM_HEAD = 64
 INTERMEDIATES = namedtuple("INTERMEDIATES", ["pre_softmax_attn", "post_softmax_attn"])
 LAYER_INTERMEDIATES = namedtuple("INTERMEDIATES", ["hiddens", "attn_intermediates"])
 
-## helpers
+## helpers => functions
 
 
 def exists(value):
@@ -43,4 +43,39 @@ def max_neg_value(tensor):
     """Returns the maximum negative value that can be represented by the data type of the tensor"""
     return -torch.finfo(tensor.dtype).max
 
+
+## helpers => classes
+
+
+class always:
+    """A callable class that always returns the init value when called"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+
+class equals:
+    """A callable class that checks if the input is equal to the init value"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, input, *args, **kwargs):
+        return input == self.value
+
+
+class not_equals:
+    """A callable class that checks if the input is not-equal to the init value"""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, input, *args, **kwargs):
+        return input != self.value
+
+
+## helpers => initialization
 
