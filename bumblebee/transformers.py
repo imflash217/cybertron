4e99ded50633f1f5ea4544e4f4e9c396ff774563
imflash217 <12234745+imflash217@users.noreply.github.com>
2021-11-05 17:15:08 -0400

    cleanup

--- bumblebee/transformers.py
+++ bumblebee/transformers.py
@@ -19,122 +19,6 @@ DEFAULT_DIM_HEAD = 64
 Intermediates = namedtuple("Intermediates", ["pre_softmax_attn", "post_softmax_attn"])
 LayerIntermediates = namedtuple("Intermediates", ["hiddens", "attn_intermediates"])
 
-## helpers => functions
-
-
-## def exists(value) -> bool:
-##     """
-##     Checks if the value exists or not
-##     """
-##     return value is not None
-##
-##
-## def default(value, default):
-##     """
-##     If the "value" does not exists then the default is returned
-##     """
-##     if exists(value):
-##         return value
-##     return default() if isfunction(default) else default
-##
-##
-## def cast_tuple(value, depth) -> tuple:
-##     """
-##     Casts a value into tuple into the required depth
-##     """
-##     return value if isinstance(value, tuple) else (val,) * depth
-##
-##
-## def max_neg_value(tensor):
-##     """Returns the maximum negative value that can be represented by the data type of the tensor"""
-##     return -torch.finfo(tensor.dtype).max
-##
-##
-## ## helpers => classes
-##
-##
-## class always:
-##     """A callable class that always returns the init value when called"""
-##
-##     def __init__(self, value):
-##         self.value = value
-##
-##     def __call__(self, *args, **kwargs):
-##         return self.value
-##
-##
-## class equals:
-##     """A callable class that checks if the input is equal to the init value"""
-##
-##     def __init__(self, value):
-##         self.value = value
-##
-##     def __call__(self, input, *args, **kwargs):
-##         return input == self.value
-##
-##
-## class not_equals:
-##     """A callable class that checks if the input is not-equal to the init value"""
-##
-##     def __init__(self, value):
-##         self.value = value
-##
-##     def __call__(self, input, *args, **kwargs):
-##         return input != self.value
-##
-##
-## ## helpers => initialization
-##
-##
-## def init_zero_(layer):
-##     """inplace intializes the weights and biases of the layer to zero"""
-##     nn.init.constant_(layer.weight, 0.0)
-##     if exists(layer.bias):
-##         nn.init.constant_(layer.bias, 0.0)
-##
-##
-## ## helpers => keyword arguments
-##
-##
-## def pick_and_pop(keys, d: dict) -> dict:
-##     """
-##     pick all (key, val) pair from dictionary 'd' for all keys in 'keys'
-##     Then, return a new dict with those picked (key, val) pair
-##     """
-##     values = list(map(lambda key: d.pop(key), keys))
-##     return dict(zip(keys, values))
-##
-##
-## def string_begins_with(prefix: str, string: str) -> bool:
-##     """
-##     Checks whether the "string" begins with the "prefix"
-##     """
-##     return string.startswith(prefix)
-##
-##
-## def group_dict_by_keys(condition, d: dict):
-##     """
-##     Groups the given dict "d" into two parts whether the keys satisfy the condition or not
-##     """
-##     return_values = [dict(), dict()]
-##     for key in d.keys():
-##         match = bool(condition(key))  ## does the keys satisfy condition?
-##         idx = int(not match)  ## idx=0 if the keys staisfy the condition else 1
-##         return_values[idx][key] = d[key]
-##     return (*return_values,)
-##
-##
-## def group_by_key_prefix(prefix, d: dict):
-##     return group_dict_by_keys(partial(string_begins_with, prefix), d)
-##
-##
-## def group_by_prefix_and_trim(prefix, d: dict):
-##     kwargs_with_prefix, kwargs = group_by_key_prefix(prefix, d)
-##     trimmer = lambda x: (x[0][len(prefix) :], x[1])
-##     kwargs_without_prefix = dict(map(trimmer, kwargs_with_prefix.items()))
-##     return kwargs_without_prefix, kwargs
-##
-
 ## helpers => initialization
 
 
@@ -630,7 +514,9 @@ class Attention(nn.Module):
 
         input_mask = None
         if any(map(exists, (mask, context_mask))):
-            q_mask = utils.default(mask, lambda: torch.ones((b, n), device=device).bool())
+            q_mask = utils.default(
+                mask, lambda: torch.ones((b, n), device=device).bool()
+            )
             k_mask = q_mask if not utils.exists(context) else context_mask
             k_mask = utils.default(
                 k_mask, lambda: torch.ones((b, k.shape[-2]), device=device).bool()
@@ -933,7 +819,12 @@ class AttentionLayers(nn.Module):
         rotary_pos_emb = None
         if utils.exists(self.rotary_pos_emb):
             max_rotary_emb_length = max(
-                list(map(lambda m: (m.shape[1] if utils.exists(m) else 0) + x.shape[1], mems))
+                list(
+                    map(
+                        lambda m: (m.shape[1] if utils.exists(m) else 0) + x.shape[1],
+                        mems,
+                    )
+                )
             )
             rotary_pos_emb = self.rotary_pos_emb(max_rotary_emb_length, x.device)
 
@@ -1156,10 +1047,14 @@ class ContinuousTransformerWrapper(nn.Module):
             else utils.always(0)
         )
         self.emb_dropout = nn.Dropout(emb_dropout)
-        self.project_in = nn.Linear(dim_in, dim) if utils.exists(dim_in) else nn.Identity()
+        self.project_in = (
+            nn.Linear(dim_in, dim) if utils.exists(dim_in) else nn.Identity()
+        )
         self.attn_layers = attn_layers
         self.norm = nn.LayerNorm(dim)
-        self.project_out = nn.Linear(dim, dim_out) if utils.exists(dim_out) else nn.Identity()
+        self.project_out = (
+            nn.Linear(dim, dim_out) if utils.exists(dim_out) else nn.Identity()
+        )
 
     def forward(
         self,
@@ -1255,13 +1150,17 @@ class Bumblebee(nn.Module):
             "dim" not in enc_kwargs and "dim" not in enc_kwargs
         ), "dimension of either encoder or decoder must be set with `dim` keyword"
 
-        enc_transformer_kwargs = utils.pick_and_pop(["num_tokens", "max_seq_len"], enc_kwargs)
+        enc_transformer_kwargs = utils.pick_and_pop(
+            ["num_tokens", "max_seq_len"], enc_kwargs
+        )
         enc_transformer_kwargs["emb_dropout"] = enc_kwargs.pop("emb_dropout", 0.0)
         enc_transformer_kwargs["num_memory_tokens"] = enc_kwargs.pop(
             "num_memory_tokens", None
         )
 
-        dec_transformer_kwargs = utils.pick_and_pop(["num_tokens", "max_seq_len"], dec_kwargs)
+        dec_transformer_kwargs = utils.pick_and_pop(
+            ["num_tokens", "max_seq_len"], dec_kwargs
+        )
         dec_transformer_kwargs["emb_dropout"] = dec_kwargs.pop("emb_dropout", 0.0)
 
         self.encoder = TransformerWrapper(
